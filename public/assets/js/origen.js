4d11512480d3102c71a6a147df02c2fb7ecbfabd
Alexis <alexis.tumino@gmail.com>
2025-02-18 11:11:37 -0300

    Modificar logica para solo leer las campañas

--- public/assets/js/origen.js
+++ public/assets/js/origen.js
@@ -8,80 +8,49 @@ function getURLParameter(name) {
 
 // Matriz de mensajes
 const mensajes = {
-    "google": {
-        "default": {
-            "titulo": "Gestiona tus obras con WhatsApp y Google Sheets",
-            "descripcion": "Registrá los ingresos y egresos de tus obras a través de Whatsapp y visualizá en tiempo real tus costos totales en Google Sheets."
-        },
-        "Sorby_Leads_Search_NB": {
-            "titulo": "Calcula materiales para la construcción en 1 minuto",
-            "descripcion": "Obtén una lista precisa de materiales con precios actualizados y optimiza tu presupuesto con WhatsApp."
-        },
-        "Sorby_Leads_Search_Brand": {
-            "titulo": "Gestiona tus obras con WhatsApp y Google Sheets",
-            "descripcion": "Registra los ingresos y egresos de tus obras a través de WhatsApp y visualiza en tiempo real tus costos totales en Google Sheets."
-        }
+    "Sorby_Leads_Search_NB": {
+        "titulo": "Calcula materiales para la construcción en 1 minuto",
+        "descripcion": "Obtén una lista precisa de materiales con precios actualizados y optimiza tu presupuesto con WhatsApp."
     },
-    "Meta": {
-        "default": {
-            "titulo": "Gestiona tus obras con WhatsApp y Google Sheets",
-            "descripcion": "Registra los ingresos y egresos de tus obras a través de WhatsApp y visualiza en tiempo real tus costos totales en Google Sheets."
-        },
-        "6617261256854": {
-            "titulo": "Gestiona tus obras con WhatsApp y Google Sheets",
-            "descripcion": "Registra y administra los movimientos de tus obras con facilidad y consulta los costos en tiempo real."
-        }
+    "Sorby_Leads_Search_Brand": {
+        "titulo": "Gestiona tus obras con WhatsApp y Google Sheets",
+        "descripcion": "Registra los ingresos y egresos de tus obras a través de WhatsApp y visualiza en tiempo real tus costos totales en Google Sheets."
     },
-    "instagram": {
-        "default": {
-            "titulo": "Gestiona tus obras con WhatsApp y Google Sheets",
-            "descripcion": "Utiliza WhatsApp para registrar ingresos y egresos y consulta en Google Sheets los costos actualizados."
-        }
+    "6617261256854": {
+        "titulo": "Gestiona tus obras con WhatsApp y Google Sheets",
+        "descripcion": "Registra y administra los movimientos de tus obras con facilidad y consulta los costos en tiempo real."
+    },
+    "default": {
+        "titulo": "Gestiona tus obras con WhatsApp y Google Sheets",
+        "descripcion": "Utiliza WhatsApp para registrar ingresos y egresos y consulta en Google Sheets los costos actualizados."
     }
 };
 
 function actualizarContenido() {
-    var source = getURLParameter('utm_source') || "default";
     var campaign = getURLParameter('utm_campaign') || "default";
-
     var title = document.getElementById("hero-title");
     var description = document.getElementById("hero-description");
 
-    var mensaje = (mensajes[source] && mensajes[source][campaign])
-        ? mensajes[source][campaign]
-        : (mensajes[source] ? mensajes[source]["default"] : { "titulo": "Gestioná las cajas de tu constructora con whatsapp", "descripcion": "Registrá los ingresos y egresos de tus obras a través de Whatsapp y visualizá en tiempo real tus costos totales en Google Sheets." });
-
-    //este es el mas complejo
-    /*
-    1ra Condicion: mensajes[source] && mensajes[source][campaign]) => ¿hay una fuente (source) y dentro de ella la campaña (campaign) con los parametros de ingresados ?
-    2da condicion: mensajes[source] ? mensajes[source]["default"] : "¡Bienvenido! Gestioná tu negocio con WhatsApp."; => Verifica solo la fuente y la campaña utiliza el default, de no encontrar ni siquiera el source usara el mensaje predeterminado.
-    */
+    var mensaje = mensajes[campaign] || mensajes["default"];
 
     if (title) title.textContent = mensaje.titulo;
     if (description) description.textContent = mensaje.descripcion;
 }
 
 function actualizarEnlaceWhatsApp() {
-    var source = getURLParameter('utm_source') || "default";
     var campaign = getURLParameter('utm_campaign') || "default";
     var baseURL = "https://api.whatsapp.com/send?phone=5491128233360&text=";
     var mensajePredeterminado = "Hola, quiero empezar con Sorby";
 
-    switch (source) {
-        case "google":
-            if (campaign === "Sorby_Leads_Search_NB") {
-                mensajePredeterminado = "Hola, quiero probar Sorby";
-            } else if (campaign === "Sorby_Leads_Search_Brand") {
-                mensajePredeterminado = "Hola, quiero probar Sorby.";
-            }
+    switch (campaign) {
+        case "Sorby_Leads_Search_NB":
+            mensajePredeterminado = "Hola, quiero probar Sorby";
             break;
-        case "Meta":
-            if (campaign === "6617261256854") {
-                mensajePredeterminado = "Hola, quiero probar SorbyData";
-            }
+        case "Sorby_Leads_Search_Brand":
+            mensajePredeterminado = "Hola, quiero probar Sorby.";
             break;
-        case "instagram":
-            mensajePredeterminado = "Hola, quiero conocer Sorby.";
+        case "6617261256854":
+            mensajePredeterminado = "Hola, quiero probar SorbyData";
             break;
         default:
             mensajePredeterminado = "Hola, quiero conocer Sorby.";
@@ -100,18 +69,17 @@ function actualizarEnlaceWhatsApp() {
     });
 }
 
-function enviarDatos(source, campaign) {
-    console.log("Enviando datos:", { source, campaign });
+function enviarDatos(campaign) {
+    console.log("Enviando datos:", { campaign });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    var source = getURLParameter('utm_source') || "default";
     var campaign = getURLParameter('utm_campaign') || "default";
-
     actualizarContenido();
     actualizarEnlaceWhatsApp();
-    enviarDatos(source, campaign);
+    enviarDatos(campaign);
 });
+
 
 
 
