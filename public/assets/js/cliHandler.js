cb73a1e8654531d356cc7890ea926f76081f67a4
Federico Maidan <fede.maidan@gmail.com>
2025-05-06 09:02:32 -0300

    mejora

--- public/assets/js/cliHandler.js
+++ public/assets/js/cliHandler.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Enviando datos a la API:", datos);
 
     try {
-        const response = await fetch("http://localhost:3000/api/CliFormPag", {
+        const response = await fetch("https://api-sorbydata.up.railway.app/api/CliFormPag", {
         method: "POST",
     headers: {
         "Content-Type": "application/json"
