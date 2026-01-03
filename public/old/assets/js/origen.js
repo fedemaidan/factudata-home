576f07b84749e962123b9fded64aa1d15f2eaa20
Federico Maidan <fede.maidan@gmail.com>
2026-01-03 14:05:55 -0300

    mejora

--- public/old/assets/js/origen.js
+++ public/old/assets/js/origen.js
@@ -52,7 +52,7 @@ function actualizarEnlaceWhatsApp() {
     const userAgent = navigator.userAgent;
 
     // var baseURL = "https://api.whatsapp.com/send?phone=5491136688822&text=";
-    var baseURL = "http://137.184.68.197:3004/api/lead/redirect_whatsapp?fbclid="+fbclid+"&utm_campaign=" + campaign + "&fbp=" + fbp + "&userAgent=" + userAgent;
+    var baseURL = "http://137.184.68.197:3051/api/lead/redirect_whatsapp?fbclid="+fbclid+"&utm_campaign=" + campaign + "&fbp=" + fbp + "&userAgent=" + userAgent;
     var mensajePredeterminado = "Hola, quiero empezar con Sorby";
 
     // switch (campaign) {
